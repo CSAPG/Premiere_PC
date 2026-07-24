76a4478328edffa18081c5a2c5a672012b5d1d37
CSAPG <cynthia.poiraultgauvin@gmail.com>
2026-07-24 23:14:09 +0200

    Add files via upload

--- js/pre-titres.js
+++ js/pre-titres.js
@@ -9,4 +9,13 @@ const PRE_TITRES = {
   '03': "Composition du système initial",
   '04': "Réactions d'oxydo-réduction",
   '05': "Description d'un fluide au repos",
+  '06': "Evolution d'un système chimique",
+  '07': "Interactions fondamentales et introductio à la notion de champs",
+  '08': "Mouvement d'un système",
+  '09': "De la structure des entités à la cohésion et à la solubilité-miscibilité d'espèces chimiques",
+  '10': "De la structure à la polarité d'une entité",
+  '11': "Aspects énergétiques des phénomènes mécaniques et électriques",
+  '12': "Conversion de l'énergie stockée dans la matière organique",
+  '13': "Structure des entités organiques et synthèses",
+  '14': "Ondes mécaniques",
 };
